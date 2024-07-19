f202b2c0f7a249a160a4ab5b0ba9d99ebaf4cdd7
RoonyVTT <votantai011205@gmail.com>
2024-07-19 12:31:52 +0700

    dathaydoi

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("Hello World and welcome VoTanTai")
 print("Chung ta khong thuoc ve nhau")
+for i in range(10):
+    if i == 9:
+        print(i, end=" ")
+    else:
+        print(i, end="+")
